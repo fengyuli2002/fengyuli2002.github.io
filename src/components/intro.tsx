2c356d0bc4b7b2b0ec6e555653fc2a57ebe0be57
Li Fengyu <fengyuli2002@gmail.com>
2022-02-13 00:16:07 -0500

    Add description

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -7,7 +7,26 @@ function Intro() {
   return (
     <div>
       <h1>Li Fengyu</h1>
-      <p>I am a first-year computer science student at Cornell University.</p>
+      <p>
+        I'm a first-year undergraduate student at Cornell University and will
+        graduate at around May 2025. This semester you can find me at the first
+        row in the lectures of CS 4780 Intro to ML. I'm strongly interested in
+        machine learning systems and computer vision. I'm actively looking for
+        research opportunities in these fields.
+      </p>
+      <p>
+        In my spare time, I build softwares on various tech stacks in a
+        student-led project team and on my own. I sometimes participate in
+        Kaggle competitions. I'm also a piano enthusiast. I mostly play the
+        pieces by Chopin.
+      </p>
+      <p>
+        <i>
+          My office hours for CS 2110 is every Monday from 16:30 to 18:30 at
+          Rhodes 405
+        </i>
+      </p>
+
       <Breadcrumb>
         <Breadcrumb.Item href="mailto:fl334@cornell.edu">
           [Email: fl334@cornell.edu]

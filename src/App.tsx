48cfd66e3189282fec94e95d0201271cdfe5db24
Li Fengyu <fengyuli2002@gmail.com>
2022-02-12 23:43:25 -0500

    Basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import Intro from "./components/intro";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Li Fengyu
-      </header>
+      <Intro></Intro>
     </div>
   );
 }
---
+++ src/components/intro.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Breadcrumb } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./intro.css";
+
+function Intro() {
+  return (
+    <div>
+      <p>Li Fengyu</p>
+      <p>I am a first-year computer science student at Cornell University.</p>
+      <Breadcrumb>
+        <Breadcrumb.Item href="mailto:fl334@cornell.edu">
+          [Email: fl334@cornell.edu]
+        </Breadcrumb.Item>
+        {/* <Breadcrumb.Item href="#">Google Scholar</Breadcrumb.Item> */}
+        <Breadcrumb.Item href="https://github.com/fengyuli2002">
+          [GitHub]
+        </Breadcrumb.Item>
+        <Breadcrumb.Item href="https://www.linkedin.com/in/fengyuli">
+          [LinkedIn]
+        </Breadcrumb.Item>
+        <Breadcrumb.Item active>[CV]</Breadcrumb.Item>
+      </Breadcrumb>
+    </div>
+  );
+}
+
+export default Intro;

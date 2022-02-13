65935284785a9740577c54f9c93e14be566a4186
Li Fengyu <fengyuli2002@gmail.com>
2022-02-13 12:34:29 -0500

    Alert for small screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Intro from "./components/intro";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -6,6 +6,14 @@ import "./App.css";
 function App() {
   return (
     <div>
+      {window.innerWidth < 768 && (
+        <div>
+          <p>
+            <i>This website is not compatible with phone-sized screens.</i>
+          </p>
+          <hr></hr>
+        </div>
+      )}
       <Intro></Intro>
     </div>
   );

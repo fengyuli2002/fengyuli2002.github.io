a0139d8a54b42a6e4f013e26665df1280962efff
Li Fengyu <fengyuli2002@gmail.com>
2022-02-18 18:07:40 -0500

    Update intro

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -8,12 +8,11 @@ function Intro() {
     <div>
       <h1>Li Fengyu</h1>
       <p>
-        I'm a first-year undergraduate student at Cornell University and will
-        graduate at around May 2025. I'm studying computer science and
-        mathematics. This semester you can find me at the first row in the
-        lectures of CS 4780 Intro to ML. I'm strongly interested in machine
-        learning systems and computer vision and I'm actively looking for
-        research opportunities in these fields.
+        I'm a first-year undergraduate student at Cornell University majoring in
+        computer science and mathematics. This semester you can find me at the
+        first row in the lectures of CS 4780 Intro to ML. I'm strongly
+        interested in machine learning systems and deep learning. I'm
+        actively looking for research opportunities in these fields.
       </p>
       <p>
         In my spare time, I build softwares on various tech stacks in a
